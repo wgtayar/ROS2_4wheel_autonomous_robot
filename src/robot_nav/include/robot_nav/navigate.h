9df8d5201dcad92ed4c917fe6e96c59ec82672c0
Wajih-Georges El Tayar <wgtayar@gmail.com>
2025-05-11 22:18:09 +0300

    Skeleton for implementing the navigation BT Node using ROS Actions.

--- src/robot_nav/include/robot_nav/navigate.h
+++ src/robot_nav/include/robot_nav/navigate.h
@@ -1,30 +1,28 @@
 #ifndef ROBOT_NAV__NAVIGATE_HPP_
 #define ROBOT_NAV__NAVIGATE_HPP_
 
-#include <array>
 #include <string>
-#include <vector>
+#include <array>
 #include <memory>
-
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-#include <std_msgs/msg/color_rgba.hpp>
 #include <geometry_msgs/msg/twist.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
-#include <robot_nav/srv/navigate.hpp>
+#include <robot_nav/action/navigate.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 
 namespace robot_nav
 {
 
+  using NavigateAction = robot_nav::action::Navigate;
+  using GoalHandleNavigate = rclcpp_action::ServerGoalHandle<NavigateAction>;
+
   struct VFFVectors
   {
-    std::vector<float> attractive;
-    std::vector<float> repulsive;
-    std::vector<float> result;
+    std::array<float, 2> attractive{}, repulsive{}, result{};
   };
 
   enum VFFColor
@@ -40,46 +38,38 @@ namespace robot_nav
     Navigate();
 
   private:
-    void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
-    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-    void handle_navigate(
-        const std::shared_ptr<robot_nav::srv::Navigate::Request> request,
-        std::shared_ptr<robot_nav::srv::Navigate::Response> response);
-    void control_cycle();
-    double calculate_distance_to_goal(double tx, double ty);
+    // action server callbacks
+    rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID &uuid,
+        std::shared_ptr<const NavigateAction::Goal> goal);
+    rclcpp_action::CancelResponse handle_cancel(
+        const std::shared_ptr<GoalHandleNavigate> goal_handle);
+    void handle_accepted(
+        const std::shared_ptr<GoalHandleNavigate> goal_handle);
+    void execute(const std::shared_ptr<GoalHandleNavigate> goal_handle);
+
+    // old helpers
     VFFVectors get_vff(const sensor_msgs::msg::LaserScan &scan);
-    visualization_msgs::msg::MarkerArray get_debug_vff(const VFFVectors &vff);
-    visualization_msgs::msg::Marker make_marker(
-        const std::vector<float> &vector,
-        VFFColor color);
+    void scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg);
+    void odom_callback(nav_msgs::msg::Odometry::SharedPtr msg);
 
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr vff_debug_pub_;
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-    rclcpp::Service<robot_nav::srv::Navigate>::SharedPtr service_;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::CallbackGroup::SharedPtr service_cb_group_;
-
-    tf2_ros::Buffer tf_buffer_;
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-
-    std::string world_frame_;
-    bool wander_;
+    double goal_tolerance_, stop_distance_;
+    bool wander_{true}, has_goal_{false};
     std::string target_frame_;
-    bool has_goal_;
-    bool goal_reached_;
-    std::array<double, 3> current_pos_;
+    std::array<double, 2> current_pos_{};
     sensor_msgs::msg::LaserScan::SharedPtr last_scan_;
 
-    double goal_tolerance_;
-    double stop_distance_;
-    double obstacle_distance_;
-    double influence_distance_;
-    double max_linear_speed_;
-    double max_angular_speed_;
+    // ROS interfaces
+    rclcpp_action::Server<NavigateAction>::SharedPtr action_server_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr debug_pub_;
+    tf2_ros::Buffer tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    rclcpp::TimerBase::SharedPtr cycle_timer_;
   };
 
-}
+} // namespace robot_nav
 
 #endif // ROBOT_NAV__NAVIGATE_HPP_
